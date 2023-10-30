947a6654a008be07ff32aa6e99e1f564c41fc9eb
Your Name <you@example.com>
2023-10-30 15:40:48 -0300

    questão 14 feita em lista de exercícios

---
+++ lista_exercícios_01/q14.py
@@ -0,0 +1,14 @@
+# item a
+print("Hello world!")
+
+#item b
+print("hello, world")
+
+# item c
+print("Hello!\n My name is John")
+
+# item d 
+print("Hello!\t My name is John")
+
+# item e
+print("Um dia, ela disse: “Estarei aqui por você”")
